f067b86b0a5fbd07bf03fca6424f5d69ab8cbff4
Elton Medeiros Pereira <eltonmpereira85@outlook.com>
2020-04-04 08:48:33 -0300

    Add project files.

---
+++ CalculadoraPintura/Calculadora.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPintura
+{
+    public class Calculadora
+    {
+        private double areaParedes;
+        private double areaTeto;
+
+        public double CalcularAreaParedes(double largura, double profundidade, double altura)
+        {
+            areaParedes = 2 * (profundidade + largura) * altura;
+            return areaParedes;
+        }
+
+        public double CalcularAreaTeto(double largura, double profundidade)
+        {
+            areaTeto = largura * profundidade;
+            return areaTeto;
+        }
+
+        public double CalcularLitragemTinta()
+        {
+            if(areaParedes == 0 && areaTeto == 0)
+            {
+                Console.WriteLine("Não é possivel calcular a Litragem necessária!");
+                Console.ReadLine();
+                Environment.Exit(1);
+            }
+            return (areaParedes + areaTeto) / 10;
+        }
+    }
+}
---
+++ CalculadoraPintura/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPintura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a largura da área?");
+            double largura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a profundidade da área?");
+            double profundidade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("A área das paredes é: ");
+            const double Altura = 2.9;
+
+            Calculadora calculadora = new Calculadora();
+            Console.WriteLine(calculadora.CalcularAreaParedes(largura, profundidade, Altura));
+
+            Console.WriteLine("A área do teto é: ");            
+            Console.WriteLine(calculadora.CalcularAreaTeto(largura, profundidade));
+
+            Console.WriteLine("A litragem de tinta necessária é: ");
+            Console.WriteLine((calculadora.CalcularLitragemTinta()));
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
